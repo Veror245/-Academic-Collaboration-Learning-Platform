f4e33cae788bb31066fc1a7de3676bcb42e581ae
Veror245 <54667276+Veror245@users.noreply.github.com>
2026-01-03 20:48:09 +0530

    added file uploads

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,44 @@
 from fastapi import FastAPI
+from backend.services.database import engine, SessionLocal, get_db
+from backend.services.models import Base, Room
 from backend.routers import auth, admin, student
 from backend.services import models, database
+from fastapi.staticfiles import StaticFiles
+import os
 
 models.Base.metadata.create_all(bind=database.engine)
 
+def seed_rooms():
+    db = SessionLocal()
+    if db.query(Room).first():
+        print("Rooms already exist. Skipping seed.")
+        db.close()
+        return
+
+    print("Seeding default rooms...")
+    rooms = [
+        Room(name="Computer Science", slug="cs"),
+        Room(name="Electronics & Comm", slug="ece"),
+        Room(name="Mechanical Eng", slug="mech"),
+        Room(name="Civil Engineering", slug="civil"),
+        Room(name="Basic Sciences (Phy/Chem/Math)", slug="science"),
+        Room(name="Management & Humanities", slug="hum"),
+    ]
+    
+    db.add_all(rooms)
+    db.commit()
+    print("Rooms added successfully!")
+    db.close()
+
 app = FastAPI()
+
+seed_rooms()
+
+UPLOAD_DIR = "static/uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+# 2. Mount the static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth.router)
 app.include_router(admin.router)
